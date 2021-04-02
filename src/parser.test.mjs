2e32450c76cc69044b9e2e1c74fe6e6772fa8ffa
Patrick Dubroy <pdubroy@gmail.com>
2021-04-02 21:14:23 +0200

    non-trivial methods

--- src/parser.test.mjs
+++ src/parser.test.mjs
@@ -2,8 +2,20 @@ import test from 'ava'
 
 import { parse } from './parser.mjs'
 
-test('method declarations', t => {
-  t.true(parse(`empty = ()`))
-  t.true(parse(`empty = primitive`))
-  t.true(parse(`do: blah with: thing = ()`))
+test('trivial method declarations', t => {
+  t.true(parse('empty = ()'))
+  t.true(parse('empty = primitive'))
+  t.true(parse('do: blah with: thing = ()'))
+})
+
+test('non-trivial method declarations', t => {
+  t.true(parse('three = (^3)'))
+  t.true(parse("greet: aPerson = (^'Hello')"))
+  t.true(parse('cold = (^-270.00)'))
+})
+
+test('binary method declarations', t => {
+  t.true(parse('+ aNumber = (^3)'))
+  t.true(parse('> aNumber = (^true)'))
+  t.true(parse('>> aNumber = (^false)'))
 })
