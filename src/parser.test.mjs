42288483b881ac6bbb870fb212a993391865a6df
Patrick Dubroy <pdubroy@gmail.com>
2021-04-02 21:53:02 +0200

    Message sends

--- src/parser.test.mjs
+++ src/parser.test.mjs
@@ -19,3 +19,10 @@ test('binary method declarations', t => {
   t.true(parse('> aNumber = (^true)'))
   t.true(parse('>> aNumber = (^false)'))
 })
+
+test('message sends', t => {
+  t.true(parse('x = (Color yellow)'), 'unary')
+  t.true(parse('x = (aPen go: 100.)'), 'keyword')
+  t.true(parse('x = (Pen new go: 100.)'), 'unary and keyword')
+  t.true(parse('x = (aPen go: 100 + 20.)'), 'keyword and binary')
+})
