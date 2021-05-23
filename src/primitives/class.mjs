3779c160cf48b7d8fd30f8d9642e518c144c265c
Patrick Dubroy <pdubroy@gmail.com>
2021-05-23 13:16:32 +0200

    Fix failures in ClassStructureTest

--- src/primitives/class.mjs
+++ src/primitives/class.mjs
@@ -16,11 +16,20 @@ export default {
       return Object.create(this._prototype)
     },
     fields () {
-      return this.$Array._new(
-        Object.keys(this._prototype)
-          .filter(isFieldName)
-          .map(name => name.slice(1))
-      )
+      const fieldNames = []
+
+      // We need to manually walk up the prototype chain because Object.keys()
+      // will see all of the global symbols ($nil, etc.) on Object._prototype.
+      let proto = this._prototype
+      while (proto !== this.$Object._prototype) {
+        for (const name of Object.getOwnPropertyNames(proto)) {
+          if (isFieldName(name)) {
+            fieldNames.push(name.slice(1)) // Strip the '$'
+          }
+        }
+        proto = Object.getPrototypeOf(proto)
+      }
+      return this.$Array._new(fieldNames.map(name => this.$String._new(name)))
     },
     methods () {
       return this.$Array._new(
