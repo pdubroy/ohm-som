b52e30ec3ce1f22058a49e3fa419d2187af6dc36
Patrick Dubroy <pdubroy@gmail.com>
2021-05-23 22:06:20 +0200

    Try to fix up the __dirname issue w/ CI actions

--- src/paths.mjs
+++ src/paths.mjs
@@ -1,6 +1,7 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export const somTestSuitePath = path.join(
   __dirname,
